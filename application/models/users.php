668ab5a65dd5ac71db5281f9cc03df1ea95dcbb2
Jack He <y2khjh@hotmail.com>
2013-08-15 22:28:19 +1000

    Convert Windows newlines to Linux newlines

---
+++ application/models/users.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Owner
+ * Date: 15/08/13
+ * Time: 7:09 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class Users extends DbAbstractTable
+{
+    protected $_name = 'users';
+}
